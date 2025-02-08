af8ae73a14b63512f2ca4c0888be380526a650c3
Youssif <youssifn.mostafa@gmail.com>
2025-02-08 23:22:46 +0200

    added google auth dep (no implement yet)

--- app/src/main/java/com/example/cheffy/CreateCustomMealFragment.java
+++ app/src/main/java/com/example/cheffy/CreateCustomMealFragment.java
@@ -8,20 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+public class CreateCustonMealFragment extends Fragment {
 
-public class NotificationFragment extends Fragment {
-
-
-
-    public NotificationFragment() {
+    public CreateCustonMealFragment() {
         // Required empty public constructor
     }
-
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        return inflater.inflate(R.layout.fragment_create_custom_meal, container, false);
     }
 }
