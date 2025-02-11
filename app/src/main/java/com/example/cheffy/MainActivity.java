7723e8717cc52ec6b22800102024da1233ddf804
Youssif <youssifn.mostafa@gmail.com>
2025-02-11 23:50:16 +0200

    fixed layout

--- app/src/main/java/com/example/cheffy/MainActivity.java
+++ app/src/main/java/com/example/cheffy/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.cheffy.features.home.view;
+package com.example.cheffy;
 
 import android.os.Bundle;
 
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.example.cheffy.R;
 
 public class MainActivity extends AppCompatActivity {
 
